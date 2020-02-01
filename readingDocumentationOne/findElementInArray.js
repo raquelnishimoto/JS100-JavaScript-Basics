c8bc9c698d808ea04c2af2b3c4bfaa3c61d35e43
Raquel Nishimoto <raquelnishimoto@gmail.com>
2020-02-02 08:51:57 +1300

    Initial commit - exercises on documention

---
+++ readingDocumentationOne/findElementInArray.js
@@ -0,0 +1,6 @@
+// How can we access the element 'and' in the array ['fish', 'and', 'chips']?
+
+
+const found = ['fish', 'and', 'chips'].find( element => element === 'and' );
+
+console.log(found);
---
+++ readingDocumentationTwo/styleGuide.js
@@ -0,0 +1,14 @@
+/*
+** In the following code snippet, find all violations of the style guide. 
+** Rewrite it so that it conforms with the guide.
+*/
+
+let iceCreamTaste = 'chocolate';
+let iceCreamDensity = 10;
+
+while (iceCreamDensity > 0) {
+  console.log('Drip...');
+  iceCreamDensity -= 1;
+}
+
+console.log('The ' + iceCreamTaste + ' ice cream melted.');
